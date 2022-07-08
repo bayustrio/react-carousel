685ab7a8de9dca18fd0697db5de1bcd1d99c6b6c
bayustrio7 <bayustrio7@gmail.com>
2022-07-09 03:03:47 +0700

    react-carousel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Data } from "./helper/Data";
+import {
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
+} from "react-icons/bs";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [index, setIndex] = useState(0);
+
+  const Length = Data.length - 1;
+
+  const handlePrev = () => {
+    const Index = index === 0 ? Length : index - 1;
+    setIndex(Index);
+  };
+
+  const handleNext = () => {
+    const Index = index === Length ? 0 : index + 1;
+    setIndex(Index);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen flex justify-center">
+      <div className="p-12 flex justify-center w-screen md:w-1/2 items-center">
+        <div className="relative w-full">
+          <div className="carousel">
+            <BsFillArrowLeftCircleFill
+              onClick={handlePrev}
+              className="absolute left-0 top-1/2 z-10"
+              size={50}
+              color="#ffff"
+            />
+            <div className="w-full ">
+              <div>
+                {index}
+                <img src={Data[index]} className={"active"} />
+              </div>
+            </div>
+            <BsFillArrowRightCircleFill
+              className="absolute right-0 top-1/2"
+              color="#ffff"
+              onClick={handleNext}
+              size={50}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/helper/Data.jsx
@@ -0,0 +1,7 @@
+import Image from "../images/images1.jpeg";
+import Image2 from "../images/images2.jpeg";
+export const Data = [
+  "https://images.unsplash.com/photo-1639054515808-6656d6245b16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1326&q=80",
+  Image,
+  Image2,
+];
